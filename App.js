04e352db050975ca993de58624960b3a8fa66557
jeonghun94 <66016555+jeonghun94@users.noreply.github.com>
2021-09-26 12:15:16 +0900

    엑스포 알림 보내기 테스트

--- App.js
+++ App.js
@@ -1,11 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Loading from './Loading';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import Loading from './Loading';
+
+// export default function App() {
+//   return (
+//     <Loading/>
+//   );
+// }
+
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+let count = 1;
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   return (
-    <Loading/>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>나의 토큰 값: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>title: {notification && notification.request.content.title} </Text>
+        <Text>body: {notification && notification.request.content.body}</Text>
+        <Text>data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="알림 보내기"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
   );
 }
 
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "새로운 알림이 도착 했습니다. 📬",
+      body: '여기 내용이 있네요😁',
+      data: { data: `조정훈은 멋져😊${count}` },
+    }
+    ,
+    trigger: { seconds: 1 },
+  });
+  count++;
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
--- Loading.js
+++ Loading.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Getting Fuckin Weather</Text>
+      <Text style={styles.text}>Getting Fuckin Weather!</Text>
     </View>
   );
 }
